34efa57dd75180c253af42bc29afd28fffe35006
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-08-21 16:30:52 +0200

    update display node using the debug node and adding mustache template.

--- display.js
+++ display.js
@@ -1,107 +1,90 @@
-/**
-
-Modified for the Thingbox
-Digital Airways 2015
-
-
-**/
-
-/**
- * Copyright 2013 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
 module.exports = function(RED) {
-    "use strict";
-    var util = require("util");
-    var debuglength = RED.settings.debugMaxLength||1000;
+  "use strict";
+  var util = require("util");
+  var events = require("events");
+  var path = require("path");
+  var Mustache = require("mustache");
 
-    function main(config) {
-        RED.nodes.createNode(this,config);
-        this.active = config.active;
-        var node = this;
+  var debuglength = RED.settings.debugMaxLength || 1000;
+  util.inspect.styles.boolean = "red";
 
-        this.on("input",function(msg) {
-			// If there is a message property, use it instead of the payload: it is human destinated (and payload in machine destinated)
-			if(msg.message)
-				msg.payload = msg.message;
-			if (msg.payload.length > debuglength) {
-				msg.payload = msg.payload.substr(0,debuglength) +" ....";
-			}
-			if (this.active) {
-				// node.warn(msg.payload);
-				sendDebug({name:this.name,topic:msg.topic,msg:msg.payload,_path:msg._path});
-            }
-        });
-    }
+  function DebugNode(n) {
+    RED.nodes.createNode(this,n);
+    this.name = n.name;
+    this.template = n.template;
+    this.severity = n.severity || 40;
+    this.active = (n.active === null || typeof n.active === "undefined") || n.active;
+    var node = this;
 
-	RED.nodes.registerType("display",main);
+    var mustache_left_part = "{"
+    var mustache_right_part = "}"
 
-    function sendDebug(msg) {
-        if (msg.msg instanceof Error) {
-            msg.msg = msg.msg.toString();
-        } else if (msg.msg instanceof Buffer) {
-            msg.msg = msg.msg.toString('hex');
-        } else if (typeof msg.msg === 'object') {
-            var seen = [];
-            if (util.isArray(msg.msg)) {
-                msg.format = "array";
-            }
-            msg.msg = JSON.stringify(msg.msg, function(key, value) {
-                if (typeof value === 'object' && value !== null) {
-                    if (seen.indexOf(value) !== -1) { return "[circular]"; }
-                    seen.push(value);
-                }
-                return value;
-            }," ");
-            seen = null;
-        } else if (typeof msg.msg === "boolean") {
-            msg.msg = msg.msg.toString();
-        } else if (typeof msg.msg === "number") {
-            msg.msg = msg.msg.toString();
-        } else if (msg.msg === 0) {
-            msg.msg = "0";
-        } else if (msg.msg === null || typeof msg.msg === "undefined") {
-            msg.msg = "(undefined)";
-        } else {
-            msg.msg = msg.msg;
+    var levels = {
+      off: 1,
+      fatal: 10,
+      error: 20,
+      warn: 30,
+      info: 40,
+      debug: 50,
+      trace: 60,
+      audit: 98,
+      metric: 99
+    };
+    var colors = {
+      "0": "grey",
+      "10": "grey",
+      "20": "red",
+      "30": "yellow",
+      "40": "grey",
+      "50": "green",
+      "60": "blue"
+    };
+
+    this.on("input",function(msg) {
+      var output = msg.payload
+      var property = 'payload'
+
+      var propertyIsFromMsg = true;
+      var showTopic = true;
+      var showFormat = true;
+
+      if(msg.message) {
+        output = msg.message;
+        property = 'message'
+      }
+      var template = `{{{${property}}}}`
+
+      if(node.template){
+        var mustache_left_part_count = 0;
+        var mustache_right_part_count = 0;
+        for (var i = 0; i < node.template.length; mustache_left_part_count += +(mustache_left_part === node.template[i++]));
+        for (var i = 0; i < node.template.length; mustache_right_part_count += +(mustache_right_part === node.template[i++]));
+        if(mustache_left_part_count === mustache_right_part_count){
+          template = node.template;
+          property = template;
+          propertyIsFromMsg = false;
+          showTopic = false;
+          showFormat = false;
         }
-
-        if (msg.msg.length > debuglength) {
-            msg.msg = msg.msg.substr(0,debuglength) +" ....";
-        }
-
-        RED.comms.publish("debug",msg);
-    }
-
-	
-
-    RED.httpAdmin.post("/debug/:id/:state", RED.auth.needsPermission("debug.write"), function(req,res) {
-        var node = RED.nodes.getNode(req.params.id);
-        var state = req.params.state;
-        if (node !== null && typeof node !== "undefined" ) {
-            if (state === "enable") {
-                node.active = true;
-                res.send(200);
-            } else if (state === "disable") {
-                node.active = false;
-                res.send(201);
-            } else {
-                res.send(404);
-            }
-        } else {
-            res.send(404);
-        }
+      }
+      output = Mustache.render(template, RED.util.cloneMessage(msg));
+      if (this.active) {
+        sendDebug({id:node.id, z:node.z, name:node.name, topic:msg.topic, property:property, msg:output, _path:msg._path, propertyIsFromMsg:propertyIsFromMsg, showTopic:showTopic, showFormat:showFormat});
+      }
     });
+  }
+  RED.nodes.registerType("display",DebugNode, {
+    settings: {
+      displayMaxLength: {
+        value: 1000
+      }
+    }
+  });
+
+  function sendDebug(msg) {
+    // don't put blank errors in sidebar (but do add to logs)
+    //if ((msg.msg === "") && (msg.hasOwnProperty("level")) && (msg.level === 20)) { return; }
+    msg = RED.util.encodeObject(msg,{maxLength:debuglength});
+    RED.comms.publish("debug",msg);
+  }
 };
